dfa93a7eb58a66c32aee7d870d9ad39f7553d5bf
Devraj Deshmukh <devrajdeshmukh112@gmail.com>
2024-10-30 00:26:25 +0530

    axios API_BASE_URL updated

--- api4forms/src/utils/api.jsx
+++ api4forms/src/utils/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://api-4forms.vercel.app/api";
 
 export const requestApiKey = async (email) => {
   try {
